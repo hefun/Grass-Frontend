022b8864a1a9bb9aa63646de10fad2cc92396d88
Yunduo <wangyunduo@buaa.edu.cn>
2022-12-09 03:13:44 +0800

    [update pages] 更新出入校审批相关页面

--- src/views/reviewLeaveSchool/index.vue
+++ src/views/reviewLeaveSchool/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <el-descriptions title="用户信息">
-    <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
-    <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-    <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
-    <el-descriptions-item label="备注">
-      <el-tag size="small">学校</el-tag>
-    </el-descriptions-item>
-    <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
-  </el-descriptions>
+  <div class="app-container">
+    <div style="margin:0 0 5px 20px">
+      <el-descriptions title="出校申请" :column="1" border>
+        <el-descriptions-item label="姓名" label-class-name="bold-label">田震</el-descriptions-item>
+        <el-descriptions-item label="学号">SY2221110<br/></el-descriptions-item>
+        <el-descriptions-item label="学院/书院">软件学院</el-descriptions-item>
+        <el-descriptions-item label="联系方式">15936022797</el-descriptions-item>
+        <el-descriptions-item label="详细事由">回家</el-descriptions-item>
+        <el-descriptions-item label="开始时间">2022-12-08 23:35:25</el-descriptions-item>
+        <el-descriptions-item label="结束时间"></el-descriptions-item>
+        <el-descriptions-item label="详细行程">G312</el-descriptions-item>
+      </el-descriptions>
+    </div>
+  </div>
 </template>
