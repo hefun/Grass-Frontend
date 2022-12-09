cf4600d136201e48c90c70ef5d3a9f02133ed89f
Yunduo <wangyunduo@buaa.edu.cn>
2022-12-09 22:12:24 +0800

    mock update 申请表 假数据更新

--- mock/review.js
+++ mock/review.js
@@ -7,14 +7,15 @@ for (let i = 0; i < count; i++) {
   List.push(Mock.mock({
     id: '@increment',
     from_id: 0,
-    to_id: 2,
-    from_name: '@cname',
     student_id: /^(BY|SY){0,1}(17|18|19|20|21|22)([0-1][0-9]|[2-4][0-2])[0-9]{3}$/,
+    from_name: '@cname',
     department: function() {
       if (Mock.Random.boolean()) return '@cword(1, 5)' + '书院'
       else return '@cword(1, 5)' + '学院'
     },
     phone: /^1(3\d|4[5-9]|5[0-35-9]|6[567]|7[0-8]|8\d|9[0-35-9])\d{8}$/,
+    to_id: 2,
+    to_name: '@cname',
     from_time: Mock.Random.datetime('yyyy/MM/dd HH:mm:ss'),
     to_time: '',
     start_time: Mock.Random.datetime('yyyy/MM/dd HH:mm:ss'),
@@ -52,7 +53,7 @@ module.exports = [
     }
   },
   {
-    url: '/review/listForm',
+    url: '/apply/listTo',
     type: 'get',
     response: config => {
       const { page = 1, limit = 10 } = config.query
@@ -64,7 +65,20 @@ module.exports = [
         total: List.length
       }
     }
+  },
+  {
+    url: '/apply/all',
+    type: 'get',
+    response: config => {
+      const { page = 1, limit = 10 } = config.query
+      const pageList = List.filter((item, index) => index < limit * page && index >= limit * (page - 1))
 
+      return {
+        code: 20000,
+        data: pageList,
+        total: List.length
+      }
+    }
   }
 ]
 
